bf4bd5598e0d64650ab121ad54f4dda8d1c562b6
Rishi <rishikm215@gmail.com>
2025-12-15 11:06:13 +0530

    added cors

--- server/index.js
+++ server/index.js
@@ -1,13 +1,21 @@
 import express from "express";
+import cors from 'cors';
+import authRoutes from './routes/auth.js'
 const app = express();
 const port = 8080;
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
 
 app.listen(port, () => {
   console.log("listening on port" + `http://localhost:${port}`);
 });
-
-app.get("/", (req, res) => {
-  res.send("working");
+app.use(cors({
+  origin:"http://localhost:5173",
+  credentials:true,
+  allowedHeaders: ["Content-Type", "Authorization"]
+}))
+app.get("/api", (req, res) => {
+  res.status(201).json("landing page");
 });
+app.use("/api",authRoutes);
